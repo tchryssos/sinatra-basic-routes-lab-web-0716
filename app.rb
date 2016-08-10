0baf649e64408091380ed1f33e82fe1ebd17df64
Troy Chryssos <github email address>
2016-08-10 18:42:08 -0400

    Wrote methods in app.rb which, depending on the users' route, return different biographical information.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is Troy"
+  end
+
+  get "/hometown" do
+    "My hometown is Katonah, NY"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is 'Weird Fishes' by Radiohead"
+  end
+
 end
